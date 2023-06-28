ddb09f75f91f2a0ba4987e483339a04e296f6636
lazarmijatovicra942018 <mijatovic.ra94.2018@uns.ac.rs>
2023-06-28 23:30:43 +0200

    implemented tests for backchaining  similar grading

--- kjar-example/drools-spring-app/src/test/java/tests/BackChainingTests.java
+++ kjar-example/drools-spring-app/src/test/java/tests/BackChainingTests.java
@@ -113,4 +113,305 @@ public class BackChainingTests {
     }
 
 
+    @Test
+    public void testSimilarGrading() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        KieBaseConfiguration kconf = ks.newKieBaseConfiguration();
+        kconf.setOption(EventProcessingOption.STREAM);
+        KieBase kieBase = kContainer.newKieBase(kconf);
+        KieSessionConfiguration kconfig1 = ks.newKieSessionConfiguration();
+        kconfig1.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
+        KieSession kieSession = kieBase.newKieSession(kconfig1, null);
+
+
+        List<String> ingrediants1= new ArrayList<>();
+        ingrediants1.add("karamela");
+        ingrediants1.add("mlecna cokolada");
+
+        Chocolate c1 = new Chocolate("Snikers", ingrediants1,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c1);
+
+
+        List<String> ingrediants2= new ArrayList<>();
+        ingrediants1.add("bela cokolada");
+        Chocolate c2 = new Chocolate("Twix", ingrediants2,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c2);
+
+
+
+        List<String> ingrediants3= new ArrayList<>();
+        ingrediants3.add("karamela");
+        ingrediants3.add("crna cokolada");
+
+        Chocolate c3 = new Chocolate("Mars", ingrediants3,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c3);
+
+
+        User user = new User("none@gmail.com","none","none", "none" , "566566" , UserType.REGISTERED_USER , UserRank.NONE);
+        kieSession.insert(user);
+
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Mars",5));
+
+        kieSession.getAgenda().getAgendaGroup("backward-chaining").setFocus();
+
+        int firedRules = kieSession.fireAllRules();
+        assertThat(1, equalTo(firedRules));
+        assertEquals(0, c1.getScore());
+        assertEquals(118, c3.getScore());
+
+        kieSession.dispose();
+
+
+    }
+
+
+    @Test
+    public void testSimilarGrading2() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        KieBaseConfiguration kconf = ks.newKieBaseConfiguration();
+        kconf.setOption(EventProcessingOption.STREAM);
+        KieBase kieBase = kContainer.newKieBase(kconf);
+        KieSessionConfiguration kconfig1 = ks.newKieSessionConfiguration();
+        kconfig1.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
+        KieSession kieSession = kieBase.newKieSession(kconfig1, null);
+
+
+        List<String> ingrediants1= new ArrayList<>();
+        ingrediants1.add("karamela");
+        ingrediants1.add("mlecna cokolada");
+
+        Chocolate c1 = new Chocolate("Snikers", ingrediants1,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c1);
+
+
+        List<String> ingrediants2= new ArrayList<>();
+        ingrediants1.add("bela cokolada");
+        Chocolate c2 = new Chocolate("Twix", ingrediants2,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c2);
+
+
+
+        List<String> ingrediants3= new ArrayList<>();
+        ingrediants3.add("karamela");
+        ingrediants3.add("crna cokolada");
+
+        Chocolate c3 = new Chocolate("Mars", ingrediants3,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c3);
+
+
+        User user = new User("none@gmail.com","none","none", "none" , "566566" , UserType.REGISTERED_USER , UserRank.NONE);
+        kieSession.insert(user);
+
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Snikers",1));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Mars",5));
+
+        kieSession.getAgenda().getAgendaGroup("backward-chaining").setFocus();
+
+        int firedRules = kieSession.fireAllRules();
+        assertThat(0, equalTo(firedRules));
+        assertEquals(0, c1.getScore());
+        assertEquals(0, c3.getScore());
+
+        kieSession.dispose();
+
+
+    }
+
+
+    @Test
+    public void testSimilarGrading3() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        KieBaseConfiguration kconf = ks.newKieBaseConfiguration();
+        kconf.setOption(EventProcessingOption.STREAM);
+        KieBase kieBase = kContainer.newKieBase(kconf);
+        KieSessionConfiguration kconfig1 = ks.newKieSessionConfiguration();
+        kconfig1.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
+        KieSession kieSession = kieBase.newKieSession(kconfig1, null);
+
+
+        List<String> ingrediants1= new ArrayList<>();
+        ingrediants1.add("karamela");
+        ingrediants1.add("mlecna cokolada");
+
+        Chocolate c1 = new Chocolate("Snikers", ingrediants1,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c1);
+
+
+        List<String> ingrediants2= new ArrayList<>();
+        ingrediants1.add("bela cokolada");
+        Chocolate c2 = new Chocolate("Twix", ingrediants2,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c2);
+
+
+
+        List<String> ingrediants3= new ArrayList<>();
+        ingrediants3.add("karamela");
+        ingrediants3.add("crna cokolada");
+
+        Chocolate c3 = new Chocolate("Mars", ingrediants3,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c3);
+
+
+        User user = new User("none@gmail.com","none","none", "none" , "566566" , UserType.REGISTERED_USER , UserRank.NONE);
+        kieSession.insert(user);
+
+        kieSession.insert(new ChocolateGrade("platinum@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Mars",5));
+
+        kieSession.getAgenda().getAgendaGroup("backward-chaining").setFocus();
+
+        int firedRules = kieSession.fireAllRules();
+        assertThat(0, equalTo(firedRules));
+        assertEquals(0, c1.getScore());
+        assertEquals(0, c3.getScore());
+
+        kieSession.dispose();
+
+
+    }
+
+
+    @Test
+    public void testSimilarGrading4() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        KieBaseConfiguration kconf = ks.newKieBaseConfiguration();
+        kconf.setOption(EventProcessingOption.STREAM);
+        KieBase kieBase = kContainer.newKieBase(kconf);
+        KieSessionConfiguration kconfig1 = ks.newKieSessionConfiguration();
+        kconfig1.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
+        KieSession kieSession = kieBase.newKieSession(kconfig1, null);
+
+
+        List<String> ingrediants1= new ArrayList<>();
+        ingrediants1.add("karamela");
+        ingrediants1.add("mlecna cokolada");
+
+        Chocolate c1 = new Chocolate("Snikers", ingrediants1,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c1);
+
+
+        List<String> ingrediants2= new ArrayList<>();
+        ingrediants1.add("bela cokolada");
+        Chocolate c2 = new Chocolate("Twix", ingrediants2,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c2);
+
+
+
+        List<String> ingrediants3= new ArrayList<>();
+        ingrediants3.add("karamela");
+        ingrediants3.add("crna cokolada");
+
+        Chocolate c3 = new Chocolate("Mars", ingrediants3,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c3);
+
+
+        User user = new User("none@gmail.com","none","none", "none" , "566566" , UserType.REGISTERED_USER , UserRank.NONE);
+        kieSession.insert(user);
+
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Mars",1));
+
+        kieSession.getAgenda().getAgendaGroup("backward-chaining").setFocus();
+
+        int firedRules = kieSession.fireAllRules();
+        assertThat(0, equalTo(firedRules));
+        assertEquals(0, c1.getScore());
+        assertEquals(0, c3.getScore());
+
+        kieSession.dispose();
+
+
+    }
+
+
+
+    @Test
+    public void testSimilarGrading5() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        KieBaseConfiguration kconf = ks.newKieBaseConfiguration();
+        kconf.setOption(EventProcessingOption.STREAM);
+        KieBase kieBase = kContainer.newKieBase(kconf);
+        KieSessionConfiguration kconfig1 = ks.newKieSessionConfiguration();
+        kconfig1.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
+        KieSession kieSession = kieBase.newKieSession(kconfig1, null);
+
+
+        List<String> ingrediants1= new ArrayList<>();
+        ingrediants1.add("karamela");
+        ingrediants1.add("mlecna cokolada");
+
+        Chocolate c1 = new Chocolate("Snikers", ingrediants1,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c1);
+
+
+        List<String> ingrediants2= new ArrayList<>();
+        ingrediants1.add("bela cokolada");
+        Chocolate c2 = new Chocolate("Twix", ingrediants2,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c2);
+
+
+
+        List<String> ingrediants3= new ArrayList<>();
+        ingrediants3.add("karamela");
+        ingrediants3.add("crna cokolada");
+
+        Chocolate c3 = new Chocolate("Mars", ingrediants3,"Mars" , 1000,30,1000,1);
+
+        kieSession.insert(c3);
+
+
+        User user = new User("none@gmail.com","none","none", "none" , "566566" , UserType.REGISTERED_USER , UserRank.NONE);
+        kieSession.insert(user);
+
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Snikers",5));
+        kieSession.insert(new ChocolateGrade("none@gmail.com","Mars",5));
+        kieSession.insert(new ChocolateGrade("silver@gmail.com","Mars",5));
+
+        kieSession.getAgenda().getAgendaGroup("backward-chaining").setFocus();
+
+        int firedRules = kieSession.fireAllRules();
+        assertThat(0, equalTo(firedRules));
+        assertEquals(0, c1.getScore());
+        assertEquals(0, c3.getScore());
+
+        kieSession.dispose();
+
+
+    }
+
+
+
+
+
+
 }
